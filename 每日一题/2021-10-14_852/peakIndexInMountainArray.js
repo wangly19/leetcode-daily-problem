7e830840780cb10d66cbe18f96bf47f0909cbeae
wangly19 <18607948581@163.com>
2021-10-14 22:28:37 +0800

    feat: 2021年10月13日 852题补充README

--- 每日一题/2021-10-14_852/peakIndexInMountainArray.js
+++ 每日一题/2021-10-14_852/peakIndexInMountainArray.js
@@ -7,13 +7,13 @@
   let left = 1
   let right = arr.length - 2
   
-  let ans = 0
+  let top = 0
 
   while(left <= right) {
       /** @name 获取中心index下标 */
       const midIndex = Math.floor((left + right) / 2)
       if (arr[midIndex] > arr[midIndex + 1]) {
-          ans = midIndex
+        top = midIndex
           right = midIndex - 1
       } else {
           left = midIndex + 1
